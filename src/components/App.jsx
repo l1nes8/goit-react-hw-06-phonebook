7f1dfe9a2f862af6f670501d725d32b43499c748
l1nes8 <126972112+l1nes8@users.noreply.github.com>
2023-10-20 19:54:22 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+export function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(stringifiedContacts) ?? [];
+    setContacts(parsedContacts);
+  }, []);
+
+  useEffect(() => {
+    const stringifiedContacts = JSON.stringify(contacts);
+    localStorage.setItem('contacts', stringifiedContacts);
+  }, [contacts]);
+
+  const addContact = (name, number) => {
+    const newContact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    const updatedContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    setContacts(updatedContacts);
+  };
+
+  const handleFilterChange = filter => {
+    setFilter(filter);
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={addContact} contacts={contacts} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
+    </div>
+  );
+}
